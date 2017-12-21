f2a57a955dda737145cd938afef71640dd8107c7
Andy Evans <andrewjamesevans@gmail.com>
2017-12-21 15:09:44 +0000

    Tidying

--- 2017/Day9/Day9-2.py
+++ 2017/Day9/Day9-2.py
@@ -4,8 +4,6 @@ with open('input.txt', 'r') as inputFile:
 	contents = inputFile.readlines()[0]
 
 	escapedContents = re.sub('!.', '', contents)
-	garbageFreeContents = re.sub('<.*?>', '', escapedContents)
-
 	garbageGroups = re.findall('<.*?>', escapedContents)
 
 	print len(''.join(garbageGroups)) - 2 * len(garbageGroups)
