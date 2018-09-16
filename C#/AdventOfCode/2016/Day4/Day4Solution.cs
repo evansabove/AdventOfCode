641614f1f29a07a95b0929a48825538acf1a828e
andrewjamesevans@gmail.com <andrewjamesevans@gmail.com>
2018-09-16 21:15:41 +0100

    Day 4 solutions

---
+++ C#/AdventOfCode/2016/Day4/Day4Solution.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using NUnit.Framework;
+using System.Text.RegularExpressions;
+using System.Text;
+
+namespace AdventOfCode
+{
+    public class Day4Solution : ISolution
+    {
+        public string Part1()
+        {
+            var input = File.ReadAllLines(Path.Combine(TestContext.CurrentContext.TestDirectory, "2016", "Day4", "input.txt"));
+
+            var sectorIdSum = 0;
+
+            foreach(var line in input)
+            {
+                var checkSum = line.Split('[', ']')[1];
+                var dashTokens = line.Split('-').ToList();
+                var encryptedName = string.Join("", dashTokens.Take(dashTokens.Count - 1));
+                var sectorId = int.Parse(Regex.Match(line, @"\d+").Value);
+
+                if (RoomIsDecoy(encryptedName, checkSum))
+                    sectorIdSum += sectorId;
+            }
+
+            return sectorIdSum.ToString();
+        }
+
+        public bool RoomIsDecoy(string encryptedName, string checksum)
+        {
+            var letters = encryptedName.GroupBy(x => x)
+                .OrderByDescending(g => g.Count())
+                .ThenBy(g => g.Key)
+                .Select(g => g.Key)
+                .Take(5)
+                .ToArray();
+
+            return checksum.Equals(new string(letters));
+        }
+
+        public string Part2()
+        {
+            var input = File.ReadAllLines(Path.Combine(TestContext.CurrentContext.TestDirectory, "2016", "Day4", "input.txt"));
+
+            foreach (var line in input)
+            {
+                var checkSum = line.Split('[', ']')[1];
+                var dashTokens = line.Split('-').ToList();
+                var encryptedName = string.Join(" ", dashTokens.Take(dashTokens.Count - 1));
+                var sectorId = int.Parse(Regex.Match(line, @"\d+").Value);
+
+                var decrypted = Shift(encryptedName, sectorId);
+                //Console.WriteLine(decrypted + " - " + sectorId);
+
+                if (decrypted.Equals("northpole object storage"))
+                    return sectorId.ToString();
+            }
+
+            return null;
+        }
+
+        public string Shift(string s, int times)
+        {
+            var newString = new StringBuilder();
+
+            foreach(var ch in s)
+            {
+                if (ch == ' ')
+                {
+                    newString.Append(ch);
+                    continue;
+                }
+
+                var newChar = (char)(ch + times);
+                
+                while(newChar > 'z')
+                {
+                    newChar = (char)(newChar - 26);
+                }
+
+                newString.Append(newChar);
+            }
+
+            return newString.ToString();
+        }
+    }
+}
--- C#/AdventOfCode/TestFixture.cs
+++ C#/AdventOfCode/TestFixture.cs
@@ -19,20 +19,40 @@ namespace AdventOfCode
             {
                 new Day1Solution(),
                 new Day2Solution(),
-                new Day3Solution()
-                
+                new Day3Solution(),
+                new Day4Solution()
+
             };
         }
 
         [Test]
         public void FullSuite()
         {
-            for(var i=0; i<_solutions.Count; i++)
+            for (var i = 0; i < _solutions.Count; i++)
             {
                 Console.WriteLine("Day {0} part {1} solution: {2}", i + 1, 1, _solutions[i].Part1());
                 Console.WriteLine("Day {0} part {1} solution: {2}", i + 1, 2, _solutions[i].Part2());
                 Console.Write(Environment.NewLine);
             }
         }
+
+        [Test]
+        public void Day4()
+        {
+            var day4 = new Day4Solution();
+            Assert.True(day4.RoomIsDecoy("aaaaabbbzyx", "abxyz"));
+            Assert.True(day4.RoomIsDecoy("abcdefgh", "abcde"));
+            Assert.True(day4.RoomIsDecoy("notarealroom", "oarel"));
+            Assert.False(day4.RoomIsDecoy("totallyrealroom", "decoy"));
+        }
+
+        [Test]
+        public void Day4_2()
+        {
+            var day4 = new Day4Solution();
+            Assert.AreEqual("cde", day4.Shift("abc", 2));
+            Assert.AreEqual("zab", day4.Shift("xyz", 2));
+        }
+
     }
 }
